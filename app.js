91330a078fc2497e480fbc75d3a122ee648d8208
EAZYFIXIT <eazyfixit@EAZYFIXITs-MacBook-Air.local>
2026-09-09 23:56:13 +0530

    Learn Async await operation

---
+++ app.js
@@ -0,0 +1,42 @@
+const cl = console.log;
+
+function snackBar(msg, icon) {
+    Swal.fire({
+        title: msg,
+        icon: icon,
+        timer: 2000
+    });
+}
+
+function ProductDetails() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let success = Math.random() < 0.5;
+
+            if (success) {
+                let msg = "Product Details fetched successfully...";
+                resolve(msg);
+            } else {
+                let error = "Something went wrong...";
+                reject(error);
+            }
+
+        }, 900);
+    });
+}
+
+async function init() {
+    try {
+        let res = await ProductDetails();
+        cl(res);
+        snackBar(res, "success");
+
+    } catch (error) {
+        cl(error);
+        snackBar(error, "error");
+    }
+}
+
+init();
+
